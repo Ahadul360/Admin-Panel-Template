d4bbb451d567c7f4c645c9353b5d14a7f0eca45f
Ahadul Islam <ahadul360@gmail.com>
2020-12-11 14:30:36 +0600

    Sidenav Content Overlay Add

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
-const menu = document.querySelector(".toggle-button");
-const sideNav = document.querySelector(".sidenav");
-const sideNavTop = document.querySelector(".sidenav-top");
+const menu = document.querySelector(".toggle-button")
+const sideNav = document.querySelector(".sidenav")
+const sideNavTop = document.querySelector(".sidenav-top")
+const contentOverlay = document.querySelector(".content-overlay")
 
 menu.onclick = () => {
   sideNav.classList.toggle("active");
+  contentOverlay.classList.toggle("active")
 };
 
 sideNavTop.onclick = () => {
   sideNav.classList.toggle("active");
+  contentOverlay.classList.toggle("active")
 };
+
+contentOverlay.onclick = () =>{
+  sideNav.classList.toggle("active");
+  contentOverlay.classList.toggle("active")
+}
 
 window.addEventListener("resize", () => {
   var w = document.documentElement.clientWidth;
   if (w <= 992) {
-    sideNav.classList.remove("active");
+    sideNav.classList.remove("active")
+    contentOverlay.classList.remove("active")
   }
 });
